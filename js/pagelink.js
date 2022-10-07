11ef6a9a7b443ec672b7f4d68594d51d04db75d7
Joanna <jjasinska.coding@gmail.com>
2022-10-07 15:07:35 +0200

    BEM -  index main part1

---
+++ js/pagelink.js
@@ -0,0 +1,7 @@
+(() => {
+  var nr = "07";
+  var link = "https://joanna-jasinska.github.io/goit-markup-hw-" + nr + "/";
+
+  document.getElementsByClassName("page-link").item(0).href = link;
+  document.getElementsByClassName("page-link").item(1).href = link;
+})();
